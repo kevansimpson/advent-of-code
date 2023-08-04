2bc9f30a4ac9647a1bab3d70ab6650816296439d
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2023-08-03 20:22:53 -0700

    cleanup Util

--- src/main/java/org/base/advent/util/Util.java
+++ src/main/java/org/base/advent/util/Util.java
@@ -38,6 +38,7 @@ public class Util {
         return result;
     }
 
+    @SafeVarargs
     public static <T> List<List<T>> permute(T... set) {
         List<List<T>> result = new ArrayList<>();
 
@@ -49,17 +50,13 @@ public class Util {
             List<List<T>> current = new ArrayList<>();
 
             for (List<T> l : result) {
-                // # of locations to insert is largest index + 1
+                // # of locations to insert is the largest index + 1
                 for (int j = 0; j < l.size() + 1; j++) {
                     // + add num[i] to different locations
                     l.add(j, t);
 
                     List<T> temp = new ArrayList<>(l);
                     current.add(temp);
-
-                    //System.out.println(temp);
-
-                    // - remove num[i] add
                     //noinspection SuspiciousListRemoveInLoop
                     l.remove(j);
                 }
@@ -70,37 +67,4 @@ public class Util {
 
         return result;
     }
-
-//    public List<List<Integer>> permute(int[] num) {
-//        List<List<Integer>> result = new ArrayList<>();
-//
-//        //start from an empty list
-//        result.add(new ArrayList<>());
-//
-//        for (int i = 0; i < num.length; i++) {
-//            //list of list in current iteration of the array num
-//            List<List<Integer>> current = new ArrayList<>();
-//
-//            for (List<Integer> l : result) {
-//                // # of locations to insert is largest index + 1
-//                for (int j = 0; j < l.size()+1; j++) {
-//                    // + add num[i] to different locations
-//                    l.add(j, num[i]);
-//
-//                    ArrayList<Integer> temp = new ArrayList<>(l);
-//                    current.add(temp);
-//
-//                    //System.out.println(temp);
-//
-//                    // - remove num[i] add
-//                    l.remove(j);
-//                }
-//            }
-//
-//            result = new ArrayList<>(current);
-//        }
-//
-//        return result;
-//    }
-
 }
