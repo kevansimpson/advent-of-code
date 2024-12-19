eca31f32f1eb7eda6ca8f083710a5423c240d2f0
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-12-18 21:19:07 -0800

    add correct timings for ParallelSolution tests

--- src/main/java/org/base/advent/ParallelSolution.java
+++ src/main/java/org/base/advent/ParallelSolution.java
@@ -3,21 +3,66 @@ package org.base.advent;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Function;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static org.base.advent.ParallelSolution.Part.*;
 import static org.base.advent.util.Util.safeGet;
 
 @AllArgsConstructor
 public abstract class ParallelSolution<T> implements Solution<T>, Function<T, Pair<Object, Object>> {
+    public static final String START_EVENT = "start";
+    public static final String STOP_EVENT = "stop";
+
+    public enum Part {
+        part1, part2, total
+    }
+
     private final ExecutorService pool;
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     @Override
     public Pair<Object, Object> apply(T input) {
-        CompletableFuture<Object> part1 = supplyAsync(() -> solvePart1(input), pool);
-        CompletableFuture<Object> part2 = supplyAsync(() -> solvePart2(input), pool);
-        return Pair.of(safeGet(part1), safeGet(part2));
+        propertyChangeSupport.firePropertyChange(
+                START_EVENT, getParallelKey(getName(), total), null);
+
+        propertyChangeSupport.firePropertyChange(
+                START_EVENT, getParallelKey(getName(), part1), null);
+        CompletableFuture<Object> future1 =
+                supplyAsync(() -> solvePart1(input), pool)
+                        .whenCompleteAsync((result, ex) ->
+                                propertyChangeSupport.firePropertyChange(
+                                        STOP_EVENT, getParallelKey(getName(), part1), result), pool);
+
+        propertyChangeSupport.firePropertyChange(
+                START_EVENT, getParallelKey(getName(), part2), null);
+        CompletableFuture<Object> future2 =
+                supplyAsync(() -> solvePart2(input), pool)
+                        .whenCompleteAsync((result, ex) ->
+                                propertyChangeSupport.firePropertyChange(
+                                        STOP_EVENT, getParallelKey(getName(), part2), result), pool);
+        try {
+            return Pair.of(safeGet(future1), safeGet(future2));
+        }
+        finally {
+            propertyChangeSupport.firePropertyChange(
+                    STOP_EVENT, getParallelKey(getName(), total), null);
+        }
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    public static String getParallelKey(String name, Part part) {
+        return String.format("%s:%s", name, part.name());
     }
 }
--- src/test/java/org/base/advent/PuzzleTester.java
+++ src/test/java/org/base/advent/PuzzleTester.java
@@ -1,12 +1,23 @@
 package org.base.advent;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import static org.base.advent.ParallelSolution.*;
+import static org.base.advent.ParallelSolution.Part.part1;
+import static org.base.advent.ParallelSolution.Part.part2;
+import static org.base.advent.ParallelSolution.Part.total;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PuzzleTester implements PuzzleReader {
@@ -34,14 +45,33 @@ public class PuzzleTester implements PuzzleReader {
         System.out.printf("%n\t-----%n");
     }
 
+    protected <T> void testParallelSolutions(ParallelSolution<T> solution,
+                                             T input,
+                                             Object expected1,
+                                             Object expected2) {
+        final String name = solution.getName();
+        PuzzleListener listener = new PuzzleListener(name);
+        solution.addPropertyChangeListener(listener);
+        Pair<Object, Object> results = solution.apply(input);
+        assertEquals(expected1, results.getLeft(), name);
+        assertEquals(expected2, results.getRight(), name);
+        Map<String, Duration> durations = listener.getDurations();
+        Duration d1 = durations.get(getParallelKey(name, part1)), d2 = durations.get(getParallelKey(name, part2));
+        Duration entire = durations.get(getParallelKey(name, total));
+        printTime(name +" part1", d1);
+        printTime(name +" part2", d2);
+        printTime(name +" total", entire);
+        System.out.printf("%n\t-----%n");
+    }
+
     protected <T> void testSolutions(Solution<T> solution,
                                      T input,
                                      Object expected1,
                                      Object expected2) {
         final String name = solution.getClass().getSimpleName();
         final long total =
-                stopwatch(name + " part 1", expected1, () -> solution.solvePart1(input))
-                + stopwatch(name + " part 2", expected2, () -> solution.solvePart2(input));
+                stopwatch(name + " part1", expected1, () -> solution.solvePart1(input))
+                + stopwatch(name + " part2", expected2, () -> solution.solvePart2(input));
         printTime(name + " total", Duration.ofNanos(total));
         System.out.printf("%n\t-----%n");
     }
@@ -52,5 +82,33 @@ public class PuzzleTester implements PuzzleReader {
         Duration duration = Duration.of(System.nanoTime() - start, ChronoUnit.NANOS);
         printTime(name, duration);
         return duration.toNanos();
+    }
+
+    @RequiredArgsConstructor
+    private static class PuzzleListener implements PropertyChangeListener {
+        Map<String, Long> startTimes = new HashMap<>();
+        Map<String, Duration> durations = new HashMap<>();
+        CountDownLatch latch = new CountDownLatch(6);
+        final String name;
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            String key = String.valueOf(evt.getOldValue());
+            switch (evt.getPropertyName()) {
+                case START_EVENT -> startTimes.put(key, System.nanoTime());
+                case STOP_EVENT -> durations.put(key, Duration.ofNanos(System.nanoTime() - startTimes.get(key)));
+            }
+            latch.countDown();
+        }
+
+        public Map<String, Duration> getDurations() {
+            try {
+                latch.await();
+                return durations;
+            }
+            catch (Exception ex) {
+                throw new RuntimeException(name, ex);
+            }
+        }
     }
 }
