e7dbfc03f178af0987cf0232e1d4fe1da837f8e3
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2025-12-25 16:36:08 -0800

    improved Day08, 2025

--- src/main/java/org/base/advent/code2025/Day08.java
+++ src/main/java/org/base/advent/code2025/Day08.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.base.advent.util.Point3D;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 /**
@@ -11,20 +12,20 @@ import java.util.function.Function;
  */
 public class Day08 implements Function<List<String>, Pair<Integer, Long>> {
     record Circuit(Point3D a, Point3D b, double distance) {}
-    record CircuitGroup(Set<Point3D> points, Set<Circuit> circuits) {
+    record CircuitGroup(Set<Point3D> points) {
         CircuitGroup() {
-            this(new HashSet<>(), new HashSet<>());
+            this(new HashSet<>());
         }
 
         CircuitGroup add(Circuit c) {
             points.add(c.a);
             points.add(c.b);
-            circuits.add(c);
             return this;
         }
     }
-    record Playground(Map<Point3D, CircuitGroup> groups, List<Circuit> list, Set<Point3D> points) {
+    record Playground(Map<Point3D, CircuitGroup> groups, List<Circuit> list, AtomicInteger points) {
         long loop(int start, int end) {
+            int remaining = points.get();
             for (int i = start; i < end; i++) {
                 Circuit c = list.get(i);
                 CircuitGroup groupA = groups.get(c.a);
@@ -34,42 +35,41 @@ public class Day08 implements Function<List<String>, Pair<Integer, Long>> {
                         CircuitGroup newGroup = new CircuitGroup().add(c);
                         groups.put(c.a, newGroup);
                         groups.put(c.b, newGroup);
+                        remaining -= 2;
                     }
                     else {
                         groupB.add(c);
                         groups.put(c.a, groupB);
-                        groups.put(c.b, groupB);
+                        remaining -= 1;
                     }
                 }
                 else {
                     groupA.add(c);
+                    groups.put(c.b, groupA);
                     if (groupB != null) { // merge
                         groupB.points.forEach(pt -> groups.put(pt, groupA));
                         groupA.points.addAll(groupB.points);
-                        groupA.circuits.addAll(groupB.circuits);
                     }
-                    groups.put(c.a, groupA);
-                    groups.put(c.b, groupA);
+                    else
+                        remaining -= 1;
                 }
 
-                points.removeAll(groups.keySet());
-                if (points.isEmpty()) {
+                if (remaining == 0)
                     return c.a.x * c.b.x;
-                }
+                else
+                    points.set(remaining);
             }
             return -1;
         }
 
         int part1() {
-            Set<CircuitGroup> circuitGroups =
-                    new TreeSet<>(Comparator.comparingInt(g -> -g.points.size()));
-            circuitGroups.addAll(groups.values());
-            int product = 1;
-            Iterator<CircuitGroup> iter = circuitGroups.iterator();
-            for (int i = 0; i < 3; i++) {
-                product *= iter.next().points.size();
-            }
-            return product;
+            return groups.values().stream()
+                    .distinct()
+                    .mapToInt(g -> g.points.size())
+                    .boxed()
+                    .sorted(Comparator.reverseOrder())
+                    .limit(3)
+                    .reduce(1, (a, b) -> a * b);
         }
 
         long part2(int start) {
@@ -86,24 +86,20 @@ public class Day08 implements Function<List<String>, Pair<Integer, Long>> {
     @Override
     public Pair<Integer, Long> apply(List<String> input) {
         List<Point3D> points = input.stream().map(Point3D::point3D).toList();
-        Set<Circuit> circuits = new TreeSet<>(Comparator.comparingDouble(Circuit::distance));
-        for (Point3D a : points) {
-            for (Point3D b : points) {
-                if (a != b) {
-                    circuits.add(new Circuit(a, b, a.euclideanDistance(b)));
-                }
+        List<Circuit> circuits = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                Point3D a = points.get(i);
+                Point3D b = points.get(j);
+                circuits.add(new Circuit(a, b, a.euclideanDistance(b)));
             }
         }
+        circuits.sort(Comparator.comparingDouble(Circuit::distance));
 
-        Map<Point3D, CircuitGroup> groups = new HashMap<>();
-        List<Circuit> list = new ArrayList<>(circuits);
-        Set<Point3D> pointSet = new HashSet<>(points);
-        Playground playground = new Playground(groups, list, pointSet);
-        CircuitGroup first = new CircuitGroup().add(list.get(0));
-        first.points.forEach(pt -> {
-            groups.put(pt, first);
-            pointSet.remove(pt);
-        });
+        Map<Point3D, CircuitGroup> groups = new HashMap<>(points.size() * 2);
+        Playground playground = new Playground(groups, circuits, new AtomicInteger(points.size() - 2));
+        CircuitGroup first = new CircuitGroup().add(circuits.get(0));
+        first.points.forEach(pt -> groups.put(pt, first));
         playground.loop(1, count);
         int part1 = playground.part1();
         long part2 = playground.part2(count);
